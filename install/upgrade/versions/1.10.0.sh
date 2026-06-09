c684e211f824db2e0eb4b22cb0fb4a59413488f4
falzo <falzo@nine.serverxi.de>
2026-06-09 10:10:13 +0200

    upstream: HestiaCP snapshot 2026-06-09

---
+++ install/upgrade/versions/1.10.0.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Hestia Control Panel upgrade script for target version 1.10.0
+
+#######################################################################################
+#######                      Place additional commands below.                   #######
+#######################################################################################
+####### upgrade_config_set_value only accepts true or false.                    #######
+#######                                                                         #######
+####### Pass through information to the end user in case of a issue or problem  #######
+#######                                                                         #######
+####### Use add_upgrade_message "My message here" to include a message          #######
+####### in the upgrade notification email. Example:                             #######
+#######                                                                         #######
+####### add_upgrade_message "My message here"                                   #######
+#######                                                                         #######
+####### You can use \n within the string to create new lines.                   #######
+#######################################################################################
+
+upgrade_config_set_value 'UPGRADE_UPDATE_WEB_TEMPLATES' 'false'
+upgrade_config_set_value 'UPGRADE_UPDATE_DNS_TEMPLATES' 'false'
+upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'false'
+upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'false'
+upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'false'
+
+# fix/file manager ignores user language
+echo "[ * ] Fix File Manager ignoring user language"
+cp -f "$HESTIA"/install/deb/filemanager/filegator/configuration.php "$HESTIA"/web/fm/configuration.php
+
+# Apply SSH config if running on Debian 13
+source /etc/os-release
+if [[ "$ID" == "debian" && "$VERSION_ID" == "13" ]]; then
+	_KEX_CONF="/etc/ssh/sshd_config.d/hestia-kex.conf"
+	_KEX_LINE="KexAlgorithms +diffie-hellman-group-exchange-sha256"
+
+	# Only create/modify the file if it doesn't already contain the correct config
+	if [[ ! -f "$_KEX_CONF" ]] || ! grep -qxF "$_KEX_LINE" "$_KEX_CONF"; then
+		echo "$_KEX_LINE" > "$_KEX_CONF"
+		"$BIN"/v-restart-service ssh
+	fi
+fi
