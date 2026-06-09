29d4cc83ce9ba6516bfc91e08d9a573def3e68b3
Falzo <falzo@nine.serverxi.defalko@guttzeit.it>
2026-06-09 12:54:10 +0200

    initial cleanup

--- docs/.vitepress/vitest.config.js
+++
@@ -1,9 +0,0 @@
-import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-	test: {
-		environment: 'jsdom',
-	},
-	plugins: [vue()],
-});
